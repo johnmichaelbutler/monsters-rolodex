6f4d68dfd2b15746bd2ef3322d20a763c3a48d1d
John Butler <johnmichaelbutler@yahoo.com>
2020-04-06 10:58:14 -0500

    Updates

---
+++ precache-manifest.509dbf248013df12bfe1751fb89dbe96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0cf4b5d8e318a119874c3b596dcc076",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2d37fb1a90948057c201",
+    "url": "/monsters-rolodex/static/css/main.65fd4d34.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d37fb1a90948057c201",
+    "url": "/monsters-rolodex/static/js/main.3d724d25.chunk.js"
+  },
+  {
+    "revision": "83fef09b6b4b91e8e383",
+    "url": "/monsters-rolodex/static/js/runtime-main.6036d2a0.js"
+  }
+]);
